543e44194bd8c4d765aa2af22685825e591881c6
Nandhi Pandiaraj <nandhirajan.pandiaraj@byjus.com>
2020-06-07 11:47:38 +0530

    Starting with TextInput and Adding inline CSS

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style ={{ padding:30 }}>
+      <View style ={{ flexDirection: "row", justifyContent:"space-between", alignItems: "center"}}>  
+        <TextInput 
+          placeholder ="Enter your Text" 
+          style= {{ 
+            borderColor:"black", 
+            borderWidth:1, 
+            padding: 10,
+            width: "80%" }}
+        />
+        <Button title ="ADD"/>
+      </View>
     </View>
   );
 }
