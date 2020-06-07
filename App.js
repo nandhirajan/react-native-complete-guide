5a1b14b1c8a2ace2375609242a4fea13602acf3f
Nandhi Pandiaraj <nandhirajan.pandiaraj@byjus.com>
2020-06-07 12:08:46 +0530

    replacing inline css with styleSheet object

--- App.js
+++ App.js
@@ -3,27 +3,31 @@ import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style ={{ padding:30 }}>
-      <View style ={{ flexDirection: "row", justifyContent:"space-between", alignItems: "center"}}>  
-        <TextInput 
-          placeholder ="Enter your Text" 
-          style= {{ 
-            borderColor:"black", 
-            borderWidth:1, 
-            padding: 10,
-            width: "80%" }}
+    <View style={styles.root}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter your Text"
+          style={styles.inputText}
         />
-        <Button title ="ADD"/>
+        <Button title="ADD" />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  root: {
+    padding: 30
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  inputText: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%"
+  }
 });
