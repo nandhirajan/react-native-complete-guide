924227128c350597533b58c202f1ce3072e0556f
Nandhi Pandiaraj <nandhirajan.pandiaraj@byjus.com>
2020-06-12 09:45:51 +0530

    Passing props between component and removed  unused styles

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //@refresh reset
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import ItemInput from './src/component/ItemInput';
 import ItemList from './src/component/ItemList';
@@ -8,36 +8,13 @@ import ItemList from './src/component/ItemList';
 const styles = StyleSheet.create({
   root: {
     padding: 30
-  },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  inputText: {
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10,
-    width: "80%"
-  },
-  itemList: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: "#ccc",
-    borderWidth: 1,
-    borderColor: "black"
   }
 });
 
 export default function App() {
-  const [inputItem, setInputItem] = useState("");
   const [itemList, setItemList] = useState([]);
 
-  onChangeItemHandler = (enteredText) => {
-    setInputItem(enteredText)
-  }
-
-  onClickHandler = () => {
+  onClickHandler = (inputItem) => {
     setItemList([...itemList, inputItem]);
     // setInputItem("");
   }
@@ -45,12 +22,10 @@ export default function App() {
   return (
     <View style={styles.root}>
       <ItemInput
-        onChangeItemHandler ={onChangeItemHandler}
-        inputItem = {inputItem}
-        onClickHandler = {onClickHandler}
+        onClickHandler={onClickHandler}
       />
 
-      <ItemList 
+      <ItemList
         itemList={itemList}
       />
     </View>
--- src/component/ItemInput.js
+++ src/component/ItemInput.js
@@ -1,33 +1,40 @@
 //@refresh reset
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, TextInput, View, Button } from 'react-native';
 
 const ItemInput = props => {
+
+    const [inputItem, setInputItem] = useState("");
+
+    onChangeItemHandler = (enteredText) => {
+        setInputItem(enteredText)
+    }
+
     return (
         <View style={styles.inputContainer}>
             <TextInput
                 placeholder="Enter your the item"
                 style={styles.inputText}
-                onChangeText={props.onChangeItemHandler}
-                value={props.inputItem}
+                onChangeText={onChangeItemHandler}
+                value={inputItem}
             />
-            <Button title="ADD" onPress={props.onClickHandler} />
+            <Button title="ADD" onPress={() => props.onClickHandler(inputItem)} />
         </View>)
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center"
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
     },
     inputText: {
-      borderColor: "black",
-      borderWidth: 1,
-      padding: 10,
-      width: "80%"
+        borderColor: "black",
+        borderWidth: 1,
+        padding: 10,
+        width: "80%"
     }
-  });
+});
 
-  
+
 export default ItemInput;
