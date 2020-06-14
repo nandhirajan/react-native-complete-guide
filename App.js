dde8e1b55b063eac95ddebe2404e0b665309c431
Nandhi Pandiaraj <nandhirajan.pandiaraj@byjus.com>
2020-06-14 13:35:20 +0530

    Working on Modal, Handling Modal Open and Close

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //@refresh reset
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 
 import ItemInput from './src/component/ItemInput';
 import ItemList from './src/component/ItemList';
@@ -13,10 +13,11 @@ const styles = StyleSheet.create({
 
 export default function App() {
   const [itemList, setItemList] = useState([]);
+  const [showModal, setShowModal] = useState(false);  
 
   onClickHandler = (inputItem) => {
     setItemList([...itemList, inputItem]);
-    // setInputItem("");
+    setShowModal(false);
   }
   
   deleteItemHandler = (item) => {
@@ -24,11 +25,18 @@ export default function App() {
     setItemList(updatedList);
   }
 
+  onClose = () => {
+    setShowModal(false);
+  }
+
   return (
     <View style={styles.root}>
+      <Button title="Add new item" onPress = {() => setShowModal(true)}/>
 
       <ItemInput
+        visible = {showModal}
         onClickHandler={onClickHandler}
+        onClose ={onClose}
       />
 
       <ItemList
--- src/component/ItemInput.js
+++ src/component/ItemInput.js
@@ -1,38 +1,47 @@
 //@refresh reset
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Modal } from 'react-native';
 
 const ItemInput = props => {
 
     const [inputItem, setInputItem] = useState("");
 
     onChangeItemHandler = (enteredText) => {
-        setInputItem(enteredText)
+        setInputItem(enteredText);
+    }
+
+    onClickHandler = (inputItem) => {
+        setInputItem("");
+        props.onClickHandler(inputItem);
     }
 
     return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                placeholder="Enter your the item"
-                style={styles.inputText}
-                onChangeText={onChangeItemHandler}
-                value={inputItem}
-            />
-            <Button title="ADD" onPress={() => props.onClickHandler(inputItem)} />
-        </View>)
+        <Modal visible={props.visible} animationType="slide">
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Enter your the item"
+                    style={styles.inputText}
+                    onChangeText={onChangeItemHandler}
+                    value={inputItem}
+                />
+                <Button title="ADD" onPress={() => onClickHandler(inputItem)} />
+                <Button title="CANCEL" color ="red" onPress={props.onClose} />
+            </View>
+        </Modal>)
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
+        flex: 1,
+        justifyContent: "center",
         alignItems: "center"
     },
     inputText: {
         borderColor: "black",
         borderWidth: 1,
         padding: 10,
-        width: "80%"
+        width: "80%",
+        marginBottom: 10
     }
 });
 
